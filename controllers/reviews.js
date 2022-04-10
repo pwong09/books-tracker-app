824c02510b921ebc643aba9aa47ac8519d7f66d9
pwong09 <pwong09@gmail.com>
2022-04-09 21:26:22 -0700

    add functionality to delete reviews

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,7 +2,8 @@ const Book = require('../models/book');
 const User = require('../models/user');
 
 module.exports = {
-    create: createReview
+    create: createReview,
+    delete: deleteReview
 }
 
 function createReview(req, res){
@@ -10,12 +11,28 @@ function createReview(req, res){
         req.body.user = req.user._id;
         req.body.userName = req.user.name;
         req.body.userAvatar = req.user.avatar;
-        console.log(req.body)
         book.reviews.push(req.body);
-        console.log('hi')
+        console.log(book.reviews)
+        console.log('this is book\'s reviews')
         book.save(function(err) {
             if (err) return res.send(err);
             res.redirect(`/books/${req.params.id}`);
         });
+    })
+}
+
+function deleteReview(req, res){
+    console.log(req.params.id)
+    console.log('this is req params id for the review')
+    Book.findOne({'reviews._id': req.params.id}).then(function(book) {
+        console.log(book);
+        const review = book.reviews.id(req.params.id);
+        if (!review.user.equals(req.user._id)) return res.redirect(`/books/${book._id}`);
+        review.remove();
+        book.save().then(function() {
+            res.redirect(`/books/${book._id}`);
+        }).catch(function(err){
+            return next(err);
+        })
     })
 }
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,5 +3,6 @@ const isLoggedIn = require('../config/auth');
 const reviewsCtrl = require('../controllers/reviews');
 
 router.post('/books/:id/reviews', isLoggedIn, reviewsCtrl.create);
+router.delete('/reviews/:id', isLoggedIn, reviewsCtrl.delete);
 
 module.exports = router;
