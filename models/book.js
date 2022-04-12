b6e13837c938650058deac178c34ed43e5692d29
pwong09 <pwong09@gmail.com>
2022-04-12 13:37:36 -0700

    add more styling

--- models/book.js
+++ models/book.js
@@ -4,7 +4,9 @@ const Schema = mongoose.Schema;
 const reviewSchema = new Schema ({
     content: String,
     rating: {type: Number, min: 1, max: 5},
-    user: {type: Schema.Types.ObjectId, ref: 'User', required: true}
+    user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    userName: String,
+    userAvatar: String
 }, {timestamps: true
 });
 
--- public/javascript/reviews.js
+++ public/javascript/reviews.js
@@ -4,13 +4,14 @@ const editReviewEl = document.querySelector('#edits-review');
 const editReviewFormEl = document.querySelector('.edit-review-form');
 const addReviewEl = document.querySelector('#adds-review');
 const newReviewFormEl = document.querySelector('.add-review-form');
-        
-editReviewEl.addEventListener('click', handleClick);
+
+if (editReviewEl) editReviewEl.addEventListener('click', handleClick);
+
 addReviewEl.addEventListener('click', handleClick);
 
 function handleClick(e) {
     if (e.target.id === 'edits-review') editReviewFormEl.style.display = 'block';
     if (e.target.id === 'adds-review') newReviewFormEl.style.display = 'block';
-    if (editReviewFormEl.style.display === 'block') editReviewEl.style.display = 'none';    
+    if (editReviewEl && editReviewFormEl.style.display === 'block') editReviewEl.style.display = 'none';    
     if (newReviewFormEl.style.display === 'block') addReviewEl.style.display = 'none';
 }
