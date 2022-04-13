d662c7439cc224d243e1e39f9f04a9cedfc679b0
pwong09 <pwong09@gmail.com>
2022-04-13 16:23:34 -0700

    responsive styling and update search keywords

--- controllers/index.js
+++ controllers/index.js
@@ -7,7 +7,7 @@ module.exports = {
 function index (req, res) {
     if (req.query.q) {
         const key = process.env.API_KEY
-        request(`https://www.googleapis.com/books/v1/volumes?q=${req.query.q}+intitle:${req.query.q}&key=${key}`,
+        request(`https://www.googleapis.com/books/v1/volumes?q=${req.query.q}+${req.query.keyword}:${req.query.q}&key=${key}`,
         function(err, response, body) {
             const searchResults = JSON.parse(body);
             const results = searchResults.items;
