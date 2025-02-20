d6720281c0b1da13c648720ffa35ba357c54b692
Pearl Wong <pearlw@adobe.com>
2025-02-20 09:47:58 -0500

    Add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+import { NavBar } from "./components/NavBar/NavBar";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <NavBar />
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import { NavBar } from "./components/NavBar/NavBar";
+
 import styles from "./page.module.css";
 
+/**
+ * Welcome page; authenticated & unauthenticated users can search for books here from the google books api
+ * It would be cool to add a section of New Releases, Top Rated, etc.
+ */
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <NavBar />
+        Hello
       </main>
       <footer className={styles.footer}>
       </footer>
